853100e6f9237edc2bfa425e0a98fc41425c2162
danielaolivares <dany.olivaresl@gmail.com>
2020-02-07 20:53:07 -0400

    commit about me

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import logoLaboratoria from '../images/labortoria.jpg';
+import perfil from "../images/perfil.JPG";
 import './styles/aboutMe.css';
 
 
@@ -10,6 +11,7 @@ class AboutMe extends React.Component {
             <div className="container-principal-about-me">
                 <div className="container-about-me">
                     <h3>Sobre mi</h3>
+                    <img src={perfil} className="init-section-avatar" alt="profile picture" />
                     <p> Desarrolladora Front-End egresada de <img src={logoLaboratoria} alt="logo laboratoria" className="logoLaboratoria" />, entusiasta de nuevos aprendizajes y desafíos. Me encanta trabajar en equipo, destaco mi responsabilidad, adaptabilidad a trabajar bajo presión, proactividad, perseverante y empatía. </p>
                 </div>
             </div>
