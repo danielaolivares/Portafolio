2f9084526155904fce0b5221ae9a68dafb4a9295
danielaolivares <dany.olivaresl@gmail.com>
2020-02-07 20:59:17 -0400

    commit agrego inicio

--- src/components/init.js
+++ src/components/init.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import "./styles/init.css";
-import perfil from "../images/perfil.JPG"
+import logo from '../images/daniela.gif';
 
 
 class Init extends React.Component {
     render() {
         return <div className="container-principal">
             <div className="init-section-name" >
-                <img src={perfil} className="init-section-avatar" alt="profile picture" />
-
+                <img src={logo} alt="" className="videoLogo" />
             </div>
         </div>
     }
