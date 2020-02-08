5780b949e631206bc6a08152f59671cc37690ecd
danielaolivares <dany.olivaresl@gmail.com>
2020-02-07 20:55:21 -0400

    commit agrego componente contactame con css

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,5 +1,9 @@
 import React from 'react';
+
+import './styles/contactMe.css'
 import iconGithub from '../images/github.svg';
+import iconlinkedIn from '../images/linkedIn.svg';
+import iconCurriculum from '../images/curriculum.svg';
 
 class ContactMe extends React.Component {
     render() {
@@ -8,9 +12,15 @@ class ContactMe extends React.Component {
                 <h3>
                     Contactame!!
                 </h3>
-                <div>
-                    <div>
-                        <a href=""></a>
+                <div className="row" id="contacMe">
+                    <div className="col-3">
+                        <a href="https://github.com/danielaolivares/Portafolio" target="_blank"><img src={iconGithub} alt="logo github" className="icon" /></a>
+                    </div>
+                    <div className="col-3">
+                        <a href="https://www.linkedin.com/in/daniela-olivares-lemunao-466b44149/" target="blank"><img src={iconlinkedIn} alt="logo linkedIn" className="icon" /></a>
+                    </div>
+                    <div className="col-3">
+                        <a href="file:///C:/Users/Dani%20Olivares/Desktop/daniela_olivares_cv2.pdf" target="blank"><img src={iconCurriculum} alt="logo curiculum" className="icon" /></a>
                     </div>
                 </div>
             </div>
