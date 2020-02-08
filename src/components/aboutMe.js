2dcd7f9ddcad2a3f508f9e977d88d277693dd96c
danielaolivares <dany.olivaresl@gmail.com>
2020-02-07 21:40:41 -0400

    commit add change component aboutMe and css aboutMe

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -9,8 +9,8 @@ class AboutMe extends React.Component {
     render() {
         return (
             <div className="container-principal-about-me">
+                <h3>Sobre mi</h3>
                 <div className="container-about-me">
-                    <h3>Sobre mi</h3>
                     <img src={perfil} className="init-section-avatar" alt="profile " />
                     <p> Desarrolladora Front-End egresada de <img src={logoLaboratoria} alt="logo laboratoria" className="logoLaboratoria" />, entusiasta de nuevos aprendizajes y desafíos. Me encanta trabajar en equipo, destaco mi responsabilidad, adaptabilidad a trabajar bajo presión, proactividad, perseverante y empatía. </p>
                 </div>
