64f5cc451380e13997690736baaddc4dbe5ad6d4
danielaolivares <53873355+danielaolivares@users.noreply.github.com>
2020-02-25 16:36:43 -0300

    add components proyects social with css (#1)

--- src/components/skill.js
+++ src/components/skill.js
@@ -12,48 +12,50 @@ import iconBootstrap from '../images/bootstrap.svg';
 import iconNpm from '../images/npm.svg';
 import iconTrello from '../images/trello.svg';
 import iconSlack from '../images/slack.svg';
-import "./styles/skill.css"
+import '../global.css'
 
 class Skill extends React.Component {
     render() {
         return (
             <div className=" container-skill">
-                <h3>Skills</h3>
-                <div className="row">
-                    <div className="col-1" >
+                <h3 className="title-skills">Skills</h3>
+                <div className="row" id="row-one">
+                    <div className="col-2" >
                         <img src={iconFirebase} alt="logo firebase" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconReact} alt="logo react" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconGithub} alt="logo github" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconHtml5} alt="logo html5" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconJS} alt="logo js" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconNode} alt="logo nodejs" className="icon" />
                     </div>
-                    <div className="col-1">
+                </div>
+                <div className="row" id="row-two">
+                    <div className="col-2">
                         <img src={iconCss} alt="logo css" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconVisual} alt="logo visual" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconBootstrap} alt="logo bootstrap" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconNpm} alt="logo npm" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconTrello} alt="logo trello" className="icon" />
                     </div>
-                    <div className="col-1">
+                    <div className="col-2">
                         <img src={iconSlack} alt="logo slack" className="icon" />
                     </div>
                 </div>
