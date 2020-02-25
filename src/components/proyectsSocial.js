3bc221126e0fb5efd5ea3da394e260429dc88ed1
danielaolivares <dany.olivaresl@gmail.com>
2020-02-25 16:30:25 -0400

    add components proyects social with css

--- src/components/proyectsSocial.js
+++ src/components/proyectsSocial.js
@@ -9,7 +9,7 @@ class Social extends React.Component {
                 <div className="container-proyect-social"  >
                     <h3 className="title-proyect-social"> R <br /> E<br />D<br /><br />S<br />O <br />C<br />I<br />A<br />L </h3>
 
-                    <p className="container-description"> Primer acercamiento a lenguaje a JavaScript (ES6), en este proyecto cree un juego para niños, las tecnologías utilizadas fueron  GitHub, Trello, HTML5, y CSS3.</p>
+                    <p className="container-description-social"> Una red social creada con React, utilizando firebase en esta red social, se puede loguear, crear post, subir fotos, dar like, etc</p>
                 </div>
             </div>
         )
