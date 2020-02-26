938b05b48c8582894d1651a38f0a38294f10bec9
danielaolivares <dany.olivaresl@gmail.com>
2020-02-25 23:05:14 -0400

    add change on index.html and css global

--- src/components/skill.js
+++ src/components/skill.js
@@ -19,48 +19,48 @@ class Skill extends React.Component {
         return (
             <div className=" container-skill">
                 <h3 className="title-skills">Skills</h3>
-                <div className="row" id="row-one">
-                    <div className="col-2" >
-                        <img src={iconFirebase} alt="logo firebase" className="icon" />
+                <div className="container-icons">
+                    <div className="row" id="row-one">
+                        <div className="col-2" >
+                            <img src={iconFirebase} alt="logo firebase" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconReact} alt="logo react" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconGithub} alt="logo github" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconHtml5} alt="logo html5" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconJS} alt="logo js" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconNode} alt="logo nodejs" className="icon" />
+                        </div>
                     </div>
-                    <div className="col-2">
-                        <img src={iconReact} alt="logo react" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconGithub} alt="logo github" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconHtml5} alt="logo html5" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconJS} alt="logo js" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconNode} alt="logo nodejs" className="icon" />
+                    <div className="row" id="row-two">
+                        <div className="col-2">
+                            <img src={iconCss} alt="logo css" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconVisual} alt="logo visual" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconBootstrap} alt="logo bootstrap" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconNpm} alt="logo npm" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconTrello} alt="logo trello" className="icon" />
+                        </div>
+                        <div className="col-2">
+                            <img src={iconSlack} alt="logo slack" className="icon" />
+                        </div>
                     </div>
                 </div>
-                <div className="row" id="row-two">
-                    <div className="col-2">
-                        <img src={iconCss} alt="logo css" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconVisual} alt="logo visual" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconBootstrap} alt="logo bootstrap" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconNpm} alt="logo npm" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconTrello} alt="logo trello" className="icon" />
-                    </div>
-                    <div className="col-2">
-                        <img src={iconSlack} alt="logo slack" className="icon" />
-                    </div>
-                </div>
-
-
             </div>
 
         )
