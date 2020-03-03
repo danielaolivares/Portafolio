11f5dba64392cdfeae71fe042906846488e2cc99
danielaolivares <dany.olivaresl@gmail.com>
2020-03-03 01:24:52 -0400

    add fix project cipher

--- src/components/proyects.js
+++ src/components/proyects.js
@@ -13,8 +13,10 @@ class Proyect extends React.Component {
                     <div className="title-proyects"><h1>Proyectos</h1>
                         <h3 className="title-proyect-cipher">CIPHER</h3>
                         <p>GitHub/Trello/HTML5/CSS3</p>
-                        <a href="https://github.com/danielaolivares/SCL011-Cipher" target="blank"><img src={iconGithub} alt="logo github" className="logo-github" /></a>
-                        <a href="https://danielaolivares.github.io/SCL011-Cipher/src/index.html" target="blank" className="direction">DEMO</a>
+                        <div className="links-to-projects">
+                            <a href="https://github.com/danielaolivares/SCL011-Cipher" target="blank"><img src={iconGithub} alt="logo github" className="logo-github" /></a>
+                            <a href="https://danielaolivares.github.io/SCL011-Cipher/src/index.html" target="blank" className="direction">DEMO</a>
+                        </div>
                     </div>
                     <div className="container-description"  >
                         <p > Primer acercamiento a lenguaje JavaScript (ES6), en este proyecto cree un juego para niños mayores de 8 años en donde deben cifrar respuestas de adivinanzas y decifrar mensajes secretos.</p>
