f483d51495721b7fac9d735905d4015bdee73af6
danielaolivares <dany.olivaresl@gmail.com>
2020-03-03 02:19:51 -0400

    add fix projec social

--- src/components/proyectsSocial.js
+++ src/components/proyectsSocial.js
@@ -8,13 +8,17 @@ class Social extends React.Component {
     render() {
         return (
             <div className="container-principal-social">
-                <div className="container-proyect-social"  >
-                    <h3 className="title-proyect-social"> R <br /> E<br />D<br /><br />S<br />O <br />C<br />I<br />A<br />L </h3>
-
-                    <p className="container-description-social"> Una red social creada para los fanático de las papas fritas, se trata de una Single-Page-Application (SPA) resposive, en donde puedes loguearte, escribir post e informar sobre tu "picada". Las tecnologias utilizadas son: HTML5 , CSS3 , Javascript , GitHub , Firebase. </p>
-                    <div><a href="https://github.com/danielaolivares/SCL011-Social-Network" target="blank"><img src={iconGithub} alt="logo github" className="logo-github" /></a></div>
-                    <div><a href="https://nataliabarriga.github.io/SCL011-Social-Network/src/index.html" target="blank" className="direction">DEMO</a></div>
+                <div className="container-description-social">
+                    <p > Una red social creada para los fanático de las papas fritas, se trata de una Single-Page-Application (SPA) resposive, en donde puedes loguearte, escribir post e informar sobre tu "picada". </p>
                     <video className="gif-social" src={imgSocial} autoPlay loop></video>
+                </div>
+                <div>
+                    <h3 className="title-proyect-social">RED SOCIAL </h3>
+                    <p>HTML5/CSS3/Javascript/GitHub/Firebase/</p>
+                    <div className="links-to-projects">
+                        <a href="https://github.com/danielaolivares/SCL011-Social-Network" target="blank"><img src={iconGithub} alt="logo github" className="logo-github" /></a>
+                        <a href="https://nataliabarriga.github.io/SCL011-Social-Network/src/index.html" target="blank" className="direction">DEMO</a>
+                    </div>
                 </div>
             </div>
         )
