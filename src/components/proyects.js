981c27499fc430e17ba8fb5d6dd8a1c27a22f578
danielaolivares <53873355+danielaolivares@users.noreply.github.com>
2020-03-02 23:02:47 -0300

    add icon github and demo, with direction (#3)

--- src/components/proyects.js
+++ src/components/proyects.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ScrollableAnchor from "react-scrollable-anchor"
 import imgCipher from '../images/cipher2.mp4';
+import iconGithub from '../images/github.svg';
 import '../global.css'
 
 class Proyect extends React.Component {
@@ -14,7 +15,10 @@ class Proyect extends React.Component {
                         <h3 className="title-proyect-cipher"> C <br /> I<br />P<br />H<br />E<br />R</h3>
 
                         <p className="container-description"> Primer acercamiento a lenguaje a JavaScript (ES6), en este proyecto cree un juego para niños, las tecnologías utilizadas fueron  GitHub, Trello, HTML5, y CSS3.</p>
+                        <div><a href="https://github.com/danielaolivares/SCL011-Cipher" target="blank"><img src={iconGithub} alt="logo github" className="logo-github" /></a></div>
+                        <div><a href="https://danielaolivares.github.io/SCL011-Cipher/src/index.html" target="blank" className="direction">DEMO</a></div>
                         <video className="gif-cipher" src={imgCipher} autoPlay loop ></video>
+
                     </div>
                 </div>
             </ScrollableAnchor>
--- src/components/proyectsSocial.js
+++ src/components/proyectsSocial.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import '../global.css'
 import imgSocial from '../images/imgSocial.mp4';
+import iconGithub from '../images/github.svg';
 
 class Social extends React.Component {
     render() {
@@ -11,6 +12,8 @@ class Social extends React.Component {
                     <h3 className="title-proyect-social"> R <br /> E<br />D<br /><br />S<br />O <br />C<br />I<br />A<br />L </h3>
 
                     <p className="container-description-social"> Una red social creada con React, utilizando firebase en esta red social, se puede loguear, crear post, subir fotos, dar like, etc</p>
+                    <div><a href="https://github.com/danielaolivares/SCL011-Social-Network" target="blank"><img src={iconGithub} alt="logo github" className="logo-github" /></a></div>
+                    <div><a href="https://nataliabarriga.github.io/SCL011-Social-Network/src/index.html" target="blank" className="direction">DEMO</a></div>
                     <video className="gif-social" src={imgSocial} autoPlay loop></video>
                 </div>
             </div>
